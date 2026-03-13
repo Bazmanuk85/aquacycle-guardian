b698b83dad529ab63c164d25f6d069ccb7066781
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-13 09:45:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-import os
+from fastapi.responses import HTMLResponse, RedirectResponse
+from database import engine, SessionLocal
+import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+
+    db = SessionLocal()
+
+    user = db.query(models.User).filter(
+        models.User.username == username
+    ).first()
+
+    if user:
+        return RedirectResponse("/dashboard", status_code=303)
+
+    new_user = models.User(username=username, password=password)
+    db.add(new_user)
+    db.commit()
+
+    return RedirectResponse("/dashboard", status_code=303)
+
+
+@app.get("/dashboard", response_class=HTMLResponse)
 def dashboard(request: Request):
     return templates.TemplateResponse(
         "dashboard.html",
         {"request": request}
     )
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
