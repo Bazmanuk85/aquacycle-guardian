df076947df5f240da0376eda059a4c69afa324d5
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-13 09:45:29 +0000

    Create models.py

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+
+    tanks = relationship("Tank", back_populates="owner")
+
+
+class Tank(Base):
+    __tablename__ = "tanks"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    volume = Column(Integer)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="tanks")
