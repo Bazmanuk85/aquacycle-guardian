f640f48e0ec21ee7d454499a9818d55bbcdbb88a
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-13 16:52:13 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,30 +1,39 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from database import Base
+from datetime import datetime
 
 
 class User(Base):
+
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
+
     username = Column(String, unique=True)
+
     password = Column(String)
 
     tanks = relationship("Tank", back_populates="owner")
 
 
 class Tank(Base):
+
     __tablename__ = "tanks"
 
     id = Column(Integer, primary_key=True)
+
     name = Column(String)
+
     volume = Column(Integer)
 
     owner_id = Column(Integer, ForeignKey("users.id"))
+
     owner = relationship("User", back_populates="tanks")
 
 
 class WaterTest(Base):
+
     __tablename__ = "water_tests"
 
     id = Column(Integer, primary_key=True)
@@ -32,28 +41,13 @@ class WaterTest(Base):
     tank_id = Column(Integer, ForeignKey("tanks.id"))
 
     ammonia = Column(String)
+
     nitrite = Column(String)
+
     nitrate = Column(String)
+
     ph = Column(String)
+
     temperature = Column(String)
 
-
-class Fish(Base):
-    __tablename__ = "fish"
-
-    id = Column(Integer, primary_key=True)
-
-    tank_id = Column(Integer, ForeignKey("tanks.id"))
-
-    species = Column(String)
-    count = Column(Integer)
-
-
-class WaterChange(Base):
-    __tablename__ = "water_changes"
-
-    id = Column(Integer, primary_key=True)
-
-    tank_id = Column(Integer, ForeignKey("tanks.id"))
-
-    percent = Column(Integer)
+    created = Column(DateTime, default=datetime.utcnow)
