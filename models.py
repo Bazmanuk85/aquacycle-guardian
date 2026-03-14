3e9dee53d2ee842b807174fb370ed67cad2d866f
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-14 18:31:18 +0000

    Update models.py

--- models.py
+++ models.py
@@ -4,17 +4,19 @@ from database import Base
 
 
 class User(Base):
+
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True)
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
     password = Column(String)
 
 
 class Tank(Base):
+
     __tablename__ = "tanks"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     tank_type = Column(String)
 
@@ -23,26 +25,28 @@ class Tank(Base):
 
 
 class WaterTest(Base):
+
     __tablename__ = "water_tests"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
 
     tank_id = Column(Integer, ForeignKey("tanks.id"))
 
-    ammonia = Column(Float)
-    nitrite = Column(Float)
-    nitrate = Column(Float)
+    ammonia = Column(Float, nullable=True)
+    nitrite = Column(Float, nullable=True)
+    nitrate = Column(Float, nullable=True)
 
-    ph = Column(Float)
-    temperature = Column(Float)
+    ph = Column(Float, nullable=True)
+    temperature = Column(Float, nullable=True)
 
     tank = relationship("Tank", back_populates="tests")
 
 
 class WaterChange(Base):
+
     __tablename__ = "water_changes"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
 
     tank_id = Column(Integer, ForeignKey("tanks.id"))
 
