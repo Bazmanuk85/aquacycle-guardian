149378cfc510318de58471712df5b7ed8c1600d5
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-14 13:43:40 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./aquacycle.db"
+DATABASE_URL = "sqlite:///./aquaguardian.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
