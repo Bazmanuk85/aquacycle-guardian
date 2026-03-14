cd278f94fba858017d0103bf564ac7f49b23ba16
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-14 15:34:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
+from database import engine
+import models
+
 from routes import auth
 from routes import tanks
 
 app = FastAPI()
+
+# Create database tables automatically
+models.Base.metadata.create_all(bind=engine)
 
 # Register routers
 app.include_router(auth.router)
