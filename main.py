8e71c1e62ff60c83afcda22faf5f4ceab400504c
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-14 15:25:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 
 from routes import auth
 from routes import tanks
 
 app = FastAPI()
 
-# Include routers
+# Register routers
 app.include_router(auth.router)
 app.include_router(tanks.router)
-
-# Static files (future use)
-app.mount("/static", StaticFiles(directory="static"), name="static")
