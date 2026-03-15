036f416289b8b06e4c3161d4b3750cf462af2ce7
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-15 20:16:22 +0000

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,21 +1,9 @@
-from fastapi import APIRouter, Request, Form, Depends
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import RedirectResponse
-from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
-
-import models
-from database import SessionLocal
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @router.get("/")
@@ -24,32 +12,24 @@ def login_page(request: Request):
 
 
 @router.post("/login")
-def login(request: Request,
-          username: str = Form(...),
-          password: str = Form(...),
-          db: Session = Depends(get_db)):
+def login(request: Request, username: str = Form(...), password: str = Form(...)):
 
-    user = db.query(models.User).filter(
-        models.User.username == username
-    ).first()
+    if username == "admin" and password == "admin":
 
-    if not user or user.password != password:
-        return templates.TemplateResponse(
-            "login.html",
-            {
-                "request": request,
-                "error": "something fishy going on please check username / password"
-            }
-        )
+        response = RedirectResponse("/dashboard", status_code=303)
+        response.set_cookie("user", username)
+        return response
 
-    response = RedirectResponse("/dashboard", status_code=303)
-    response.set_cookie("user", username)
-
-    return response
+    return templates.TemplateResponse(
+        "login.html",
+        {"request": request, "error": "Invalid login"}
+    )
 
 
 @router.get("/logout")
 def logout():
+
     response = RedirectResponse("/", status_code=303)
     response.delete_cookie("user")
+
     return response
