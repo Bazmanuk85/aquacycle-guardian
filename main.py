879ffcd6967ed18ceb16288829698199f5970a20
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-15 19:16:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-from database import engine
 import models
+from database import engine
 
 from routes import auth
 from routes import tanks
 
 app = FastAPI()
 
-# Create database tables automatically
 models.Base.metadata.create_all(bind=engine)
 
-# Register routers
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+# Routers
 app.include_router(auth.router)
 app.include_router(tanks.router)
