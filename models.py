50a92d83c60dc83c8a2967494ec74adbc8b0b5a6
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-15 20:15:41 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
+
 from database import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
     password = Column(String)
 
@@ -15,19 +16,15 @@ class User(Base):
 class Tank(Base):
     __tablename__ = "tanks"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     tank_type = Column(String)
-
-    tests = relationship("WaterTest", back_populates="tank")
-    changes = relationship("WaterChange", back_populates="tank")
 
 
 class WaterTest(Base):
     __tablename__ = "water_tests"
 
-    id = Column(Integer, primary_key=True, index=True)
-
+    id = Column(Integer, primary_key=True)
     tank_id = Column(Integer, ForeignKey("tanks.id"))
 
     ammonia = Column(Float, nullable=True)
@@ -38,18 +35,12 @@ class WaterTest(Base):
 
     timestamp = Column(DateTime, default=datetime.utcnow)
 
-    tank = relationship("Tank", back_populates="tests")
-
 
 class WaterChange(Base):
     __tablename__ = "water_changes"
 
-    id = Column(Integer, primary_key=True, index=True)
-
+    id = Column(Integer, primary_key=True)
     tank_id = Column(Integer, ForeignKey("tanks.id"))
 
     percent = Column(Float)
-
     timestamp = Column(DateTime, default=datetime.utcnow)
-
-    tank = relationship("Tank", back_populates="changes")
