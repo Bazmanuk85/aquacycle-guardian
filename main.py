f28d82e7cfcfdcd5c2a9dfa8c8918382e31d2b7d
Bazmanuk85 <sidecar_uk@yahoo.com>
2026-03-17 13:42:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
-from database import engine
-import models
+from database import Base, engine
 
 from routes import auth
 from routes import tanks
 
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# Create database tables
+Base.metadata.create_all(bind=engine)
 
+# Routers
 app.include_router(auth.router)
 app.include_router(tanks.router)
+
+
+# Static files (if you have css/js)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# Root redirect
+@app.get("/")
+def root():
+    return RedirectResponse(url="/login")
